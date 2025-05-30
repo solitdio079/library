71f48a0d4c9b3a5978ea452e4f618cc7296afadc
solitdio079 <solitdio079@gmail.com>
2025-05-30 05:35:34 +0300

    Finish the switch read btn

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const cardContainerEl = document.querySelector("#card-container")
 const bodyEl = document.querySelector("body")
 
-const myLibrary = []
+const openDialogBtnEl = document.querySelector("#openDialogBtn")
+const closeDialogBtnEl = document.querySelector("#closeDialogBtn")
+const formDialogEl = document.querySelector("#formDialog")
+
+const addBookForm = document.querySelector("#formDialog form")
+
+
+const myLibrary = localStorage.getItem("books") ? JSON.parse(localStorage.getItem("books")): []
+
+localStorage.getItem("books") ? "":localStorage.setItem("books", JSON.stringify(myLibrary))
+
+if(localStorage.getItem("books")){
+    myLibrary.forEach(item => {
+        Object.setPrototypeOf(item, Book.prototype)
+    })
+}
 
 
 function Book(name,author,pages,read=false){
@@ -12,19 +27,31 @@ function Book(name,author,pages,read=false){
     this.read = read
 }
 
+Book.prototype.toggleRead = function () {
+    this.read = !this.read
+}
+
 function addBookToLibrary(name,author,pages,read=false){
     const newBook = new Book(name,author,pages,read)
     myLibrary.push(newBook)
+    localStorage.setItem("books", JSON.stringify(myLibrary))
 }
-
-addBookToLibrary("Mein Kampf","Adolf Hitler",470,read=false)
-addBookToLibrary("The Stranger","George Schimdth",270,read=true)
-addBookToLibrary("Moonwalking with Einstein","Joshua Foer",170,read=true)
-addBookToLibrary("Unscripted","MJ DeMarco",513,read=true)
 listBooks()
 function listBooks(){
+    cardContainerEl.innerHTML = ""
    
     myLibrary.forEach(item => {
+
+        // the delete button
+        const deleteBtnEl = document.createElement("button")
+        deleteBtnEl.setAttribute("data-delete", item.id)
+        deleteBtnEl.textContent = "X"
+        deleteBtnEl.className = "btn bg-error rounded-none text-white text-sm"
+
+        deleteBtnEl.style.margin = "5px 10px"
+
+
+
         const bookCardEl = document.createElement("div")
         bookCardEl.classList.add("card")
 
@@ -48,7 +75,13 @@ function listBooks(){
         cardPagesEl.textContent = item.pages + " pages"
 
         const cardReadEl = document.createElement("p")
-        cardReadEl.textContent = `Read: ${item.read ? "Yes": "No"} `
+        cardReadEl.textContent = `Read: ${item.read ? "Yes": "No"}`
+
+        const readSwitchBtn = document.createElement("button")
+        readSwitchBtn.setAttribute("data-readSwitch", item.id)
+        readSwitchBtn.textContent = "Switch"
+        readSwitchBtn.className = "btn rounded-none text-sm text-white bg-primary"
+        cardReadEl.appendChild(readSwitchBtn)
 
         cardBodyEl.appendChild(cardTitleEl)
         cardBodyEl.appendChild(cardAuthorEl)
@@ -56,6 +89,7 @@ function listBooks(){
         cardBodyEl.appendChild(cardReadEl)
 
         cardImgEl.appendChild(placeholderImgEl)
+        bookCardEl.append(deleteBtnEl)
         bookCardEl.appendChild(cardImgEl)
         bookCardEl.appendChild(cardBodyEl)
         cardContainerEl.appendChild(bookCardEl)
@@ -65,3 +99,47 @@ function listBooks(){
 
     })
 }
+
+openDialogBtnEl.addEventListener("click", ()=> {
+    formDialogEl.showModal()
+})
+closeDialogBtnEl.addEventListener("click", () => {
+    formDialogEl.close()
+})
+
+addBookForm.addEventListener("submit", function (e){
+    e.preventDefault()
+    console.log("Form Submitted!")
+
+    const formData = new FormData(addBookForm)
+    const bodyObject = Object.fromEntries(formData)
+    
+    addBookToLibrary(bodyObject.name, bodyObject.author, +bodyObject.pages, Boolean(bodyObject.read))
+    listBooks()
+    addBookForm.reset()
+
+
+})
+
+cardContainerEl.addEventListener("click", function(e) {
+    console.log(e.target)
+    const deleteId = e.target.getAttribute("data-delete")
+    const switchId = e.target.getAttribute("data-readSwitch")
+    if(deleteId){
+        console.log(deleteId)
+        const deleteIndex = myLibrary.findIndex(item => item.id === deleteId)
+        myLibrary.splice(deleteIndex,1)
+        localStorage.setItem("books", JSON.stringify(myLibrary))
+        listBooks()
+    }
+
+    if(switchId){
+        console.log(switchId)
+        const switchIndex = myLibrary.findIndex(item => item.id === switchId)
+        console.log(myLibrary[switchIndex])
+        myLibrary[switchIndex].toggleRead()
+        localStorage.setItem("books", JSON.stringify(myLibrary))
+        listBooks()
+
+    }
+})
